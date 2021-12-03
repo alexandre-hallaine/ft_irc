6417942bef8a02616fd1921fa1828190018f9649
Alexandre Hallaine <hallainealexandre@gmail.com>
2021-12-03 11:56:26 +0100

    add: replies

--- src/User/replies.cpp
+++ src/User/replies.cpp
@@ -1,70 +1,79 @@
 #include "User.hpp"
 
-std::string RPL_NONE();
-std::string RPL_USERHOST(std::string reply, std::string space);
-std::string RPL_ISON(std::string nick, std::string space);
-std::string RPL_AWAY(std::string nick, std::string message);
-std::string RPL_UNAWAY();
-std::string RPL_NOWAWAY();
-std::string RPL_WHOISUSER(std::string nick, std::string user, std::string host, std::string real);
-std::string RPL_WHOISSERVER(std::string nick, std::string server, std::string info);
-std::string RPL_WHOISOPERATOR(std::string nick);
-std::string RPL_WHOISIDLE(std::string nick, std::string integer);
-std::string RPL_ENDOFWHOIS(std::string nick);
-std::string RPL_WHOISCHANNELS(std::string nick, std::string channel, std::string space);
-std::string RPL_WHOWASUSER(std::string nick, std::string user, std::string host, std::string real);
-std::string RPL_ENDOFWHOWAS(std::string nick);
-std::string RPL_LISTSTART();
-std::string RPL_LIST(std::string channel, std::string topic);
-std::string RPL_LISTEND();
-std::string RPL_CHANNELMODEIS(std::string channel, std::string mode, std::string params);
-std::string RPL_NOTOPIC(std::string channel);
-std::string RPL_TOPIC(std::string channel, std::string topic);
-std::string RPL_INVITING(std::string channel, std::string nick);
-std::string RPL_SUMMONING(std::string user);
-std::string RPL_VERSION(std::string version, std::string debuglevel, std::string server, std::string comments);
-std::string RPL_WHOREPLY(std::string channel, std::string user, std::string host, std::string server, std::string nick, std::string hopcout, std::string real);
-std::string RPL_ENDOFWHO(std::string name);
-std::string RPL_NAMREPLY(std::string channel, std::string nick_list);
-std::string RPL_ENDOFNAMES(std::string channel);
-std::string RPL_LINKS(std::string mask, std::string server, std::string hopcount, std::string info);
-std::string RPL_ENDOFLINKS(std::string mask);
-std::string RPL_BANLIST(std::string channel, std::string banip);
-std::string RPL_ENDOFBANLIST(std::string channel);
-std::string RPL_INFO(std::string string);
-std::string RPL_ENDOFINFO();
-std::string RPL_MOTDSTART(std::string server);
-std::string RPL_MOTD(std::string text);
-std::string RPL_ENDOFMOTD();
-std::string RPL_YOUREOPER();
-std::string RPL_REHASHING(std::string file);
-std::string RPL_TIME(std::string server, std::string time);
-std::string RPL_USERSSTART();
-std::string RPL_USERS();
-std::string RPL_ENDOFUSERS();
-std::string RPL_NOUSERS();
-std::string RPL_TRACELINK(std::string version, std::string destination, std::string server);
-std::string RPL_TRACECONNECTING(std::string class1, std::string server);
-std::string RPL_TRACEHANDSHAKE(std::string class1, std::string server);
-std::string RPL_TRACEUNKNOWN(std::string class1, std::string ip);
-std::string RPL_TRACEOPERATOR(std::string class1, std::string nick);
-std::string RPL_TRACEUSER(std::string class1, std::string nick);
-std::string RPL_TRACESERVER(std::string class1, std::string int1, std::string int2, std::string server, std::string nick, std::string host);
-std::string RPL_TRACENEWTYPE(std::string type, std::string name);
-std::string RPL_TRACELOG(std::string file, std::string level);
-std::string RPL_STATSLINKINFO(std::string name, std::string q, std::string smessages, std::string sbytes, std::string rsmessages, std::string rbytes, std::string time);
-std::string RPL_STATSCOMMANDS(std::string command, std::string count);
-std::string RPL_STATSCLINE(std::string host, std::string name, std::string port, std::string class1);
-std::string RPL_STATSNLINE(std::string host, std::string name, std::string port, std::string class1);
-std::string RPL_STATSILINE(std::string host1, std::string host2, std::string port, std::string class1);
-std::string RPL_STATSKLINE(std::string host, std::string user, std::string port, std::string class1);
-std::string RPL_STATSYLINE(std::string class1, std::string ping, std::string connect, std::string max);
-std::string RPL_ENDOFSTATS(std::string letter);
-std::string RPL_STATSLLINE(std::string host, std::string name, std::string depth);
-std::string RPL_STATSUPTIME();
-std::string RPL_STATSOLINE(std::string mask, std::string name);
-std::string RPL_STATSHLINE(std::string mask, std::string name);
-std::string RPL_UMODEIS(std::string mode);
+std::string RPL_TRACELINK(std::string version, std::string destination, std::string server) { return "Link " + version + " " + destination + " " + server; }
+std::string RPL_TRACECONNECTING(std::string class1, std::string server) { return "Try. " + class1 + " " + server; }
+std::string RPL_TRACEHANDSHAKE(std::string class1, std::string server) { return "H.S. " + class1 + " " + server; }
+std::string RPL_TRACEUNKNOWN(std::string class1, std::string ip) { return "???? " + class1 + " " + ip; }
+std::string RPL_TRACEOPERATOR(std::string class1, std::string nick) { return "Oper " + class1 + " " + nick; }
+std::string RPL_TRACEUSER(std::string class1, std::string nick) { return "User " + class1 + " " + nick; }
+std::string RPL_TRACESERVER(std::string class1, std::string int1, std::string int2, std::string server, std::string nick, std::string host) { return "Serv " + class1 + " " + int1 + "S " + int2 + "C " + server + " " + nick + "*!*@ " + host; }
+std::string RPL_TRACENEWTYPE(std::string type, std::string name) { return type + " 0 " + name; }
+std::string RPL_STATSLINKINFO(std::string name, std::string q, std::string smessages, std::string sbytes, std::string rsmessages, std::string rbytes, std::string time) { return name + " " + q + " " + smessages + " " + sbytes + " " + rsmessages + " " + rbytes + " " + time; }
+std::string RPL_STATSCOMMANDS(std::string command, std::string count) { return command + " " + count; }
+std::string RPL_STATSCLINE(std::string host, std::string name, std::string port, std::string class1) { return "C " + host + " * " + name + " " + port + " " + class1; }
+std::string RPL_STATSNLINE(std::string host, std::string name, std::string port, std::string class1) { return "N " + host + " * " + name + " " + port + " " + class1; }
+std::string RPL_STATSILINE(std::string host1, std::string host2, std::string port, std::string class1) { return "I " + host1 + " * " + host2 + " " + port + " " + class1; }
+std::string RPL_STATSKLINE(std::string host, std::string user, std::string port, std::string class1) { return "K " + host + " * " + user + " " + port + " " + class1; }
+std::string RPL_STATSYLINE(std::string class1, std::string ping, std::string connect, std::string max) { return "Y " + class1 + " " + ping + " " + connect + " " + max; }
+std::string RPL_ENDOFSTATS(std::string letter) { return letter + " :End of /STATS report"; }
+std::string RPL_UMODEIS(std::string mode) { return mode; }
+std::string RPL_STATSLLINE(std::string host, std::string name, std::string depth) { return "L " + host + " * " + name + " " + depth; }
+std::string RPL_STATSUPTIME() { return ":Server Up %d days %d:%02d:%02d"; }
+std::string RPL_STATSOLINE(std::string mask, std::string name) { return "O " + mask + " * " + name; }
+std::string RPL_STATSHLINE(std::string mask, std::string name) { return "H " + mask + " * " + name; }
+std::string RPL_LUSERCLIENT(std::string int1, std::string int2, std::string int3) { return ":There are " + int1 + " users and " + int2 + " invisible on " + int3 + " servers"; }
+std::string RPL_LUSEROP(std::string int1) { return int1 + " :operator(s) online"; }
+std::string RPL_LUSERUNKNOWN(std::string int1) { return int1 + " :unknown connection(s)"; }
+std::string RPL_LUSERCHANNELS(std::string int1) { return int1 + " :channels formed"; }
+std::string RPL_LUSERME(std::string int1, std::string int2) { return ":I have " + int1 + " clients and " + int2 + " servers"; }
+std::string RPL_ADMINME(std::string server) { return server + " :Administrative info"; }
+std::string RPL_ADMINLOC1(std::string info) { return ":" + info; }
+std::string RPL_ADMINLOC2(std::string info) { return ":" + info; }
+std::string RPL_ADMINEMAIL(std::string info) { return ":" + info; }
+std::string RPL_TRACELOG(std::string file, std::string level) { return "File " + file + " " + level; }
+std::string RPL_NONE() { return ""; }
+std::string RPL_AWAY(std::string nick, std::string message) { return nick + " :" + message; }
+std::string RPL_USERHOST(std::string reply_list) { return ":" + reply_list; }
+std::string RPL_ISON(std::string nick_list) { return ":" + nick_list; }
+std::string RPL_UNAWAY() { return ":You are no longer marked as being away"; }
+std::string RPL_NOWAWAY() { return ":You have been marked as being away"; }
+std::string RPL_WHOISUSER(std::string nick, std::string user, std::string host, std::string real) { return nick + " " + user + " " + host + " * :" + real; }
+std::string RPL_WHOISSERVER(std::string nick, std::string server, std::string info) { return nick + " " + server + " :" + info; }
+std::string RPL_WHOISOPERATOR(std::string nick) { return nick + " :is an IRC operator"; }
+std::string RPL_WHOWASUSER(std::string nick, std::string user, std::string host, std::string real) { return nick + " " + user + " " + host + " * :" + real; }
+std::string RPL_ENDOFWHO(std::string name) { return name + " :End of /WHO list"; }
+std::string RPL_WHOISIDLE(std::string nick, std::string integer) { return nick + " " + integer + " :seconds idle"; }
+std::string RPL_ENDOFWHOIS(std::string nick) { return nick + " :End of /WHOIS list"; }
+std::string RPL_WHOISCHANNELS(std::string nick, std::string channel) { return nick + " :" + channel; }
+std::string RPL_LISTSTART() { return "Channel :Users  Name"; }
+std::string RPL_LIST(std::string channel, std::string topic) { return channel + " :" + topic; }
+std::string RPL_LISTEND() { return ":End of /LIST"; }
+std::string RPL_CHANNELMODEIS(std::string channel, std::string mode, std::string params) { return channel + " " + mode + " " + params; }
+std::string RPL_NOTOPIC(std::string channel) { return channel + " :No topic is set"; }
+std::string RPL_TOPIC(std::string channel, std::string topic) { return channel + " :" + topic; }
+std::string RPL_INVITING(std::string channel, std::string nick) { return channel + " " + nick; }
+std::string RPL_SUMMONING(std::string user) { return user + " :Summoning user to IRC"; }
+std::string RPL_VERSION(std::string version, std::string debuglevel, std::string server, std::string comments) { return version + "." + debuglevel + " " + server + " :" + comments; }
+std::string RPL_WHOREPLY(std::string channel, std::string user, std::string host, std::string server, std::string nick, std::string hopcout, std::string real) { return channel + " " + user + " " + host + " " + server + " " + nick + " <H|G>[*][@|+] :" + hopcout + " " + real; }
+std::string RPL_NAMREPLY(std::string channel, std::string nick_list) { return channel + " :" + nick_list; }
+std::string RPL_ENDOFNAMES(std::string channel) { return channel + " :End of /NAMES list"; }
+std::string RPL_LINKS(std::string mask, std::string server, std::string hopcount, std::string info) { return mask + " " + server + " :" + hopcount + " " + info; }
+std::string RPL_ENDOFLINKS(std::string mask) { return mask + " :End of /LINKS list"; }
+std::string RPL_BANLIST(std::string channel, std::string banip) { return channel + " " + banip; }
+std::string RPL_ENDOFBANLIST(std::string channel) { return channel + " :End of channel ban list"; }
+std::string RPL_ENDOFWHOWAS(std::string nick) { return nick + " :End of WHOWAS"; }
+std::string RPL_INFO(std::string string) { return ":" + string; }
+std::string RPL_MOTD(std::string text) { return ":- " + text; }
+std::string RPL_ENDOFINFO() { return ":End of /INFO list"; }
+std::string RPL_MOTDSTART(std::string server) { return ":- " + server + " Message of the day - "; }
+std::string RPL_ENDOFMOTD() { return ":End of /MOTD command"; }
+std::string RPL_YOUREOPER() { return ":You are now an IRC operator"; }
+std::string RPL_REHASHING(std::string file) { return file + " :Rehashing"; }
+std::string RPL_TIME(std::string server, std::string time) { return server + " :" + time; }
+std::string RPL_USERSSTART() { return ":UserID   Terminal  Host"; }
+std::string RPL_USERS() { return ":%-8s %-9s %-8s"; }
+std::string RPL_ENDOFUSERS() { return ":End of users"; }
+std::string RPL_NOUSERS() { return ":Nobody logged in"; }
 
 std::string ERR_NOSUCHNICK(std::string nickname) { return nickname + " :No such nick/channel"; }
 std::string ERR_NOSUCHSERVER(std::string server) { return server + " :No such server"; }
@@ -113,13 +122,157 @@ std::string ERR_USERSDONTMATCH() { return ":Cant change mode for other users"; }
 
 std::string irc::User::generate(unsigned short code, std::string arg1, std::string arg2, std::string arg3, std::string arg4, std::string arg5, std::string arg6, std::string arg7)
 {
-	(void)arg3;
-	(void)arg4;
-	(void)arg5;
-	(void)arg6;
-	(void)arg7;
 	switch (code)
 	{
+	case 200:
+		return RPL_TRACELINK(arg1, arg2, arg3);
+	case 201:
+		return RPL_TRACECONNECTING(arg1, arg2);
+	case 202:
+		return RPL_TRACEHANDSHAKE(arg1, arg2);
+	case 203:
+		return RPL_TRACEUNKNOWN(arg1, arg2);
+	case 204:
+		return RPL_TRACEOPERATOR(arg1, arg2);
+	case 205:
+		return RPL_TRACEUSER(arg1, arg2);
+	case 206:
+		return RPL_TRACESERVER(arg1, arg2, arg3, arg4, arg5, arg6);
+	case 208:
+		return RPL_TRACENEWTYPE(arg1, arg2);
+	case 211:
+		return RPL_STATSLINKINFO(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+	case 212:
+		return RPL_STATSCOMMANDS(arg1, arg2);
+	case 213:
+		return RPL_STATSCLINE(arg1, arg2, arg3, arg4);
+	case 214:
+		return RPL_STATSNLINE(arg1, arg2, arg3, arg4);
+	case 215:
+		return RPL_STATSILINE(arg1, arg2, arg3, arg4);
+	case 216:
+		return RPL_STATSKLINE(arg1, arg2, arg3, arg4);
+	case 218:
+		return RPL_STATSYLINE(arg1, arg2, arg3, arg4);
+	case 219:
+		return RPL_ENDOFSTATS(arg1);
+	case 221:
+		return RPL_UMODEIS(arg1);
+	case 241:
+		return RPL_STATSLLINE(arg1, arg2, arg3);
+	case 242:
+		return RPL_STATSUPTIME();
+	case 243:
+		return RPL_STATSOLINE(arg1, arg2);
+	case 244:
+		return RPL_STATSHLINE(arg1, arg2);
+	case 251:
+		return RPL_LUSERCLIENT(arg1, arg2, arg3);
+	case 252:
+		return RPL_LUSEROP(arg1);
+	case 253:
+		return RPL_LUSERUNKNOWN(arg1);
+	case 254:
+		return RPL_LUSERCHANNELS(arg1);
+	case 255:
+		return RPL_LUSERME(arg1, arg1);
+	case 256:
+		return RPL_ADMINME(arg1);
+	case 257:
+		return RPL_ADMINLOC1(arg1);
+	case 258:
+		return RPL_ADMINLOC2(arg1);
+	case 259:
+		return RPL_ADMINEMAIL(arg1);
+	case 261:
+		return RPL_TRACELOG(arg1, arg2);
+	case 300:
+		return RPL_NONE();
+	case 301:
+		return RPL_AWAY(arg1, arg2);
+	case 302:
+		return RPL_USERHOST(arg1);
+	case 303:
+		return RPL_ISON(arg1);
+	case 305:
+		return RPL_UNAWAY();
+	case 306:
+		return RPL_NOWAWAY();
+	case 311:
+		return RPL_WHOISUSER(arg1, arg2, arg3, arg4);
+	case 312:
+		return RPL_WHOISSERVER(arg1, arg2, arg3);
+	case 313:
+		return RPL_WHOISOPERATOR(arg1);
+	case 314:
+		return RPL_WHOWASUSER(arg1, arg2, arg3, arg4);
+	case 315:
+		return RPL_ENDOFWHO(arg1);
+	case 317:
+		return RPL_WHOISIDLE(arg1, arg2);
+	case 318:
+		return RPL_ENDOFWHOIS(arg1);
+	case 319:
+		return RPL_WHOISCHANNELS(arg1, arg2);
+	case 321:
+		return RPL_LISTSTART();
+	case 322:
+		return RPL_LIST(arg1, arg2);
+	case 323:
+		return RPL_LISTEND();
+	case 324:
+		return RPL_CHANNELMODEIS(arg1, arg2, arg3);
+	case 331:
+		return RPL_NOTOPIC(arg1);
+	case 332:
+		return RPL_TOPIC(arg1, arg2);
+	case 341:
+		return RPL_INVITING(arg1, arg2);
+	case 342:
+		return RPL_SUMMONING(arg1);
+	case 351:
+		return RPL_VERSION(arg1, arg2, arg3, arg4);
+	case 352:
+		return RPL_WHOREPLY(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+	case 353:
+		return RPL_NAMREPLY(arg1, arg2);
+	case 366:
+		return RPL_ENDOFNAMES(arg1);
+	case 364:
+		return RPL_LINKS(arg1, arg2, arg3, arg4);
+	case 365:
+		return RPL_ENDOFLINKS(arg1);
+	case 367:
+		return RPL_BANLIST(arg1, arg2);
+	case 368:
+		return RPL_ENDOFBANLIST(arg1);
+	case 369:
+		return RPL_ENDOFWHOWAS(arg1);
+	case 371:
+		return RPL_INFO(arg1);
+	case 372:
+		return RPL_MOTD(arg1);
+	case 374:
+		return RPL_ENDOFINFO();
+	case 375:
+		return RPL_MOTDSTART(arg1);
+	case 376:
+		return RPL_ENDOFMOTD();
+	case 381:
+		return RPL_YOUREOPER();
+	case 382:
+		return RPL_REHASHING(arg1);
+	case 391:
+		return RPL_TIME(arg1, arg2);
+	case 392:
+		return RPL_USERSSTART();
+	case 393:
+		return RPL_USERS();
+	case 394:
+		return RPL_ENDOFUSERS();
+	case 395:
+		return RPL_NOUSERS();
+
 	case 401:
 		return ERR_NOSUCHNICK(arg1);
 	case 402:
