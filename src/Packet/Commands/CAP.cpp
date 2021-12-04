f3a303b95a065aa43ec7500960e872329d960bb4
Assxios <dimitriroge@hotmail.fr>
2021-12-04 01:59:39 +0100

    added MOTD handling

--- src/Packet/Commands/CAP.cpp
+++ src/Packet/Commands/CAP.cpp
@@ -1,6 +1,8 @@
 #include "../PacketManager.hpp"
 #include <sstream>
 #include <ctime>
+#include <fstream>
+#include <vector>
 
 void irc::CAP(struct irc::packetParams params)
 {
@@ -12,5 +14,30 @@ void irc::CAP(struct irc::packetParams params)
 		params.user->write(2, params.server->getServername(), params.server->getVersion());
 		params.user->write(3, params.server->getTime());
 		params.user->write(4, params.server->getServername(), params.server->getVersion(), params.user->getUser_modes(), "");
+
+
+		std::ifstream file("MOTD.txt");
+		params.user->write(375, params.server->getServername());
+		if (file.is_open())
+		{
+			std::string line;
+			std::vector<std::string> motd;
+			while (getline(file, line))
+			{
+				if (line.size() > 80)
+				{
+					params.user->write(422);
+					params.user->write(376);
+					return ;
+				}	
+				motd.push_back(line);
+			}
+			for (std::vector<std::string>::iterator it = motd.begin(); it != motd.end(); ++it)
+				params.user->write(372, it->c_str());	
+			file.close();
+		}
+		else
+			params.user->write(422);
+		params.user->write(376);
 	}
 }
