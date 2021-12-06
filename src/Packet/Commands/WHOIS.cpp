666b66d2cdfa7cbfb9d62ccf3c7eba2abcf97312
Nimon77 <nsimon@student.42.fr>
2021-12-07 00:44:35 +0100

    fix whois

--- src/Packet/Commands/WHOIS.cpp
+++ src/Packet/Commands/WHOIS.cpp
@@ -2,7 +2,7 @@
 
 void irc::WHOIS(struct irc::packetParams params)
 {
-	params.user->write(311, params.user->getUsername(), params.user->getHostname(), params.user->getHote(), params.user->getRealname());
-	params.user->write(318, params.user->getUsername());
+	params.user->write(311, params.args[1], params.user->getHostname(), params.user->getHote(), params.user->getRealname());
+	params.user->write(318, params.args[1]);
 	return ;
 }
