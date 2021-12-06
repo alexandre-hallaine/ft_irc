f6f9f2b2f7d920efc1b7958b6034bf81fab41396
Nimon77 <nsimon@student.42.fr>
2021-12-07 00:20:26 +0100

    delete CAP

--- src/Packet/Commands/CAP.cpp
+++ src/Packet/Commands/CAP.cpp
@@ -1,42 +1,7 @@
 #include "../PacketManager.hpp"
-#include <sstream>
-#include <ctime>
-#include <fstream>
-#include <vector>
 
 void irc::CAP(struct irc::packetParams params)
 {
-	if (params.args[1] == "LS")
-		params.user->write("CAP * LS :\r\n");
-	else if (params.args[1] == "END")
-	{
-		params.user->write(1, params.user->getNickname(), params.user->getUsername(), params.user->getHostname());
-		params.user->write(2, params.server->getServername(), params.server->getVersion());
-		params.user->write(3, params.server->getTime());
-		params.user->write(4, params.server->getServername(), params.server->getVersion(), params.user->getUser_modes(), "");
-
-
-		std::ifstream file("MOTD.txt");
-		params.user->write(375, params.server->getServername());
-		if (file.is_open())
-		{
-			std::string line;
-			std::vector<std::string> motd;
-			while (getline(file, line))
-			{
-				if (line.size() > 80)
-				{
-					params.user->write(422);
-					return params.user->write(376);
-				}	
-				motd.push_back(line);
-			}
-			for (std::vector<std::string>::iterator it = motd.begin(); it != motd.end(); ++it)
-				params.user->write(372, it->c_str());	
-			file.close();
-		}
-		else
-			params.user->write(422);
-		params.user->write(376);
-	}
+	(void)params;
+	return ;
 }
--- src/Packet/Commands/NICK.cpp
+++ src/Packet/Commands/NICK.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 void irc::NICK(struct irc::packetParams params)
 {
@@ -16,11 +17,42 @@ void irc::NICK(struct irc::packetParams params)
 	if (params.args[1].size() > 9)
 		return params.user->write(432, params.args[1]);	
 
-	//if (params.user->getNickname() != "*")
-	//{
-	std::stringstream ss;
-	ss << ":" << params.user->getNickname() << " " << "NICK" << " " << params.args[1] << "\r\n";
-	params.user->write(ss.str());
-	//}
+	bool init = false;
+	if (params.user->getNickname() != "*")
+	{
+		std::stringstream ss;
+		ss << ":" << params.user->getNickname() << " " << "NICK" << " " << params.args[1] << "\r\n";
+		params.user->write(ss.str());
+	}
+	else
+		init = true;
 	params.user->setNickname(params.args[1]);
+	if (init)
+	{
+		params.user->write(1, params.user->getNickname(), params.user->getUsername(), params.user->getHostname());
+		params.user->write(2, params.server->getServername(), params.server->getVersion());
+		params.user->write(3, params.server->getTime());
+		params.user->write(4, params.server->getServername(), params.server->getVersion(), params.user->getUser_modes(), "");
+
+
+		std::ifstream file("MOTD.txt");
+		params.user->write(375, params.server->getServername());
+		if (file.is_open())
+		{
+			std::string line;
+			std::vector<std::string> motd;
+			while (getline(file, line))
+			{
+				if (line.size() > 80)
+				{
+					params.user->write(422);
+					return params.user->write(376);
+				}	
+				motd.push_back(line);
+			}
+			for (std::vector<std::string>::iterator it = motd.begin(); it != motd.end(); ++it)
+				params.user->write(372, it->c_str());
+			file.close();
+		}
+	}
 }
