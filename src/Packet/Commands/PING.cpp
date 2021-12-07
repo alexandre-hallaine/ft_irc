a8d200748b2a9588176fe75499c8c1f30411c99c
Assxios <droge@student.42.fr>
2021-12-07 01:24:38 +0100

    add PING error response

--- src/Packet/Commands/PING.cpp
+++ src/Packet/Commands/PING.cpp
@@ -3,6 +3,8 @@
 
 void irc::PING(struct irc::packetParams params)
 {
+	if (params.args.size() == 1)
+		return params.user->write(409);
 	std::stringstream ss;
 	ss << "PONG " << params.args[1] << "\r\n";
 	params.user->write(ss.str());
