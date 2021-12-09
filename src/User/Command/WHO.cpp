567aa30f5dec71454c13d500302c0c8ed6e9fad3
Assxios <droge@student.42.fr>
2021-12-09 23:09:16 +0100

    nevermind hostname not needed

--- src/User/Command/WHO.cpp
+++ src/User/Command/WHO.cpp
@@ -5,7 +5,6 @@
 
 void WHO(class irc::Command *command)
 {
-	//missing is hostname
 	bool is_star = false;
 	size_t pos = 0;
 
@@ -15,10 +14,10 @@ void WHO(class irc::Command *command)
 	std::vector<irc::User *> users = command->getServer().getUsers();
 	for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); it++)
 	{	// need to check channel first
-		if ("missing" == command->getParameters()[0] || (*it)->getRealname() == command->getParameters()[0] || (*it)->getNickname() == command->getParameters()[0])
-			command->reply(352, "*", command->getUser().getUsername(), command->getUser().getHost(), "missing", command->getUser().getNickname(), "0", command->getUser().getRealname());
-		else if (is_star && (command->getParameters()[0].substr(0, pos) == std::string("missing").substr(0, pos) || command->getParameters()[0].substr(0, pos) == (*it)->getRealname().substr(0, pos) || command->getParameters()[0].substr(0, pos) == (*it)->getNickname().substr(0, pos) ))
-			command->reply(352, "*", command->getUser().getUsername(), command->getUser().getHost(), "missing", command->getUser().getNickname(), "0", command->getUser().getRealname());
+		if ((*it)->getHost() == command->getParameters()[0] || (*it)->getRealname() == command->getParameters()[0] || (*it)->getNickname() == command->getParameters()[0])
+			command->reply(352, "*", command->getUser().getUsername(), command->getUser().getHost(), "", command->getUser().getNickname(), "0", command->getUser().getRealname());
+		else if (is_star && (command->getParameters()[0].substr(0, pos) == command->getHost().substr(0, pos) || command->getParameters()[0].substr(0, pos) == (*it)->getRealname().substr(0, pos) || command->getParameters()[0].substr(0, pos) == (*it)->getNickname().substr(0, pos) ))
+			command->reply(352, "*", command->getUser().getUsername(), command->getUser().getHost(), "", command->getUser().getNickname(), "0", command->getUser().getRealname());
 	}
 	command->reply(315, command->getUser().getUsername());
 }
