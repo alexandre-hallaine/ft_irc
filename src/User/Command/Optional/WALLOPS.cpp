5d8498129c94eb075aa051140459102497a95190
Assxios <droge@student.42.fr>
2021-12-13 21:25:36 +0100

    finished WALLOPS

--- src/User/Command/Optional/WALLOPS.cpp
+++ src/User/Command/Optional/WALLOPS.cpp
@@ -1,12 +1,14 @@
 #include "../Command.hpp"
 #include "../../User.hpp"
 #include "../../../Server/Server.hpp"
-#include <iostream>
 
 void WALLOPS(irc::Command *command) 
 {
 	if (command->getParameters().size() == 0)
 		return command->reply(461, "WALLOPS");
+
+	if (command->getUser().getMode().find('o') == std::string::npos)
+		return ;
 
 	std::vector<irc::User *> users = command->getServer().getUsers();
 	for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); it++)
