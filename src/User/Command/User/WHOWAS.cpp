9e7f14a4ce7825494918543f229595e6170a54a2
Assxios <droge@student.42.fr>
2021-12-13 22:10:52 +0100

    finished WHOWAS

--- src/User/Command/User/WHOWAS.cpp
+++ src/User/Command/User/WHOWAS.cpp
@@ -5,22 +5,43 @@
 
 void WHOWAS(class irc::Command *command)
 {
-	size_t pos = 0, tmp = 0;
-	bool has_print = false;
+	size_t pos = 0, tmp = 0, count = 0;
+	bool has_print = false, has_count = false;
 
 	if (command->getParameters().size() == 0)
 		return command->reply(431);
 
+	if (command->getParameters().size() > 1)
+	{
+		for (size_t index = 0; index != command->getParameters()[1].length(); index++)
+		{
+			if (!irc::isDigit(command->getParameters()[1][index]))
+				break ;
+			count += count * 10;
+			count += command->getParameters()[1][index] - 48;
+			has_count = true;
+		}
+	}
+	
 	std::vector<irc::User *> users = command->getServer().getUsers();
 	for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); it++)
 	{
-		while ((tmp = (*it)->getPastnick().find(command->getParameters()[0], pos)) != std::string::npos)
+		if (!has_count || (has_count && count != 0))
 		{
-			command->reply(314, command->getParameters()[0], (*it)->getUsername(), (*it)->getHostname(), (*it)->getRealname());
-			pos = tmp + 1;
-			has_print = true;
+			while ((tmp = (*it)->getPastnick().find(command->getParameters()[0], pos)) != std::string::npos)
+			{
+				command->reply(314, command->getParameters()[0], (*it)->getUsername(), (*it)->getHostname(), (*it)->getRealname());
+				has_print = true;
+				if (has_count && count != 0)
+					count--;
+				if (has_count && count == 0)
+					break ;
+				pos = tmp + 1;
+			}
+			pos = 0, tmp = 0;
 		}
-		pos = 0, tmp = 0;
+		else
+			break ;
 	}
 
 	if (!has_print)
