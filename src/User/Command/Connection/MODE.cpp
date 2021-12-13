0fcb268682929f48028f0ce4d0b91c50147d597f
Assxios <droge@student.42.fr>
2021-12-13 20:55:32 +0100

    another one

--- src/User/Command/Connection/MODE.cpp
+++ src/User/Command/Connection/MODE.cpp
@@ -97,7 +97,7 @@ void MODE_channel(class irc::Command *command)
 				if (command->getUser().getMode().find("o") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("O") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("o") != std::string::npos)
 					check_mode(&mode, request[i], is_minus);
 				else
-					command->reply(482);
+					command->reply(482, command->getParameters()[0]);
 			}
 			else if (command->getParameters().size() == count && (request[i] != 'l' || !is_minus) && command->getServer().getConfig().get("channel_setmode").find(request[i]) != std::string::npos)
 				command->reply(461, "MODE");
@@ -106,7 +106,7 @@ void MODE_channel(class irc::Command *command)
 				if (command->getUser().getMode().find("o") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("O") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("o") != std::string::npos)
 					check_setmode(&mode, request[i], is_minus, command, count);
 				else
-					command->reply(482);
+					command->reply(482, command->getParameters()[0]);
 				if (request[i] != 'l')
 					count++;
 				else if (request[i] == 'l' && !is_minus)
@@ -119,7 +119,7 @@ void MODE_channel(class irc::Command *command)
 				if (request[i] != 'O' && (command->getUser().getMode().find("o") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("O") != std::string::npos || command->getServer().getChannel(command->getParameters()[0]).getUserMode(command->getUser()).find("o") != std::string::npos))
 					check_givemode(request[i], is_minus, command, count);
 				else
-					command->reply(482);
+					command->reply(482, command->getParameters()[0]);
 				count++;
 			}
 		}
