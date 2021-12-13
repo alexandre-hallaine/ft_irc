076e31ac10f2be8b0eab7ebab99c4f29bec7e470
Nimon77 <nsimon@student.42.fr>
2021-12-13 18:27:40 +0100

    handle wildcard LIST

--- src/User/Command/Channel/LIST.cpp
+++ src/User/Command/Channel/LIST.cpp
@@ -1,15 +1,16 @@
 #include "../Command.hpp"
 #include "../../../Server/Server.hpp"
+#include "../../../Utils/Utils.hpp"
 #include "../../User.hpp"
 #include <iostream>
 
 void LIST(irc::Command *command)
 {
 	std::vector<irc::Channel *> channels = command->getServer().getChannels();
-	if (command->getParameters().size() == 2)
+	if (command->getParameters().size() == 1 && command->getParameters()[0] != "")
 	{
 		for (std::vector<irc::Channel *>::iterator it = channels.begin(); it != channels.end(); ++it)
-			if ((*it)->getName() == command->getParameters()[0])
+			if (irc::strmatch((*it)->getName(), command->getParameters()[0]))
 				command->reply(322, (*it)->getName(), (*it)->getTopic());
 	}
 	else
