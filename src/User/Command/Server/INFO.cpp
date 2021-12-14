8881b1bcaeceb7762b5c87eaa86df8030acf2dcc
Alexandre Hallaine <hallainealexandre@gmail.com>
2021-12-14 10:28:41 +0100

    add: info

--- src/User/Command/Server/INFO.cpp
+++ src/User/Command/Server/INFO.cpp
@@ -1,3 +1,11 @@
 #include "../Command.hpp"
+#include "../../../Server/Server.hpp"
+#include "../../../Utils/Utils.hpp"
 
-void INFO(irc::Command *command) { (void)command; }
+void INFO(irc::Command *command)
+{
+	std::vector<std::string> motd = irc::split(command->getServer().getConfig().get("info"), "\n");
+	for (std::vector<std::string>::iterator it = motd.begin(); it != motd.end(); ++it)
+		command->reply(371, *it);
+	command->reply(374);
+}
