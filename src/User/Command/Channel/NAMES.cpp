5adb2d3ab74432d30b840813d05ef940b5f94d9a
Nimon77 <nsimon@student.42.fr>
2021-12-14 12:32:37 +0100

    add NAMES

--- src/User/Command/Channel/NAMES.cpp
+++ src/User/Command/Channel/NAMES.cpp
@@ -1,3 +1,78 @@
 #include "../Command.hpp"
+#include <algorithm>
+#include "../../../Server/Server.hpp"
+#include "../../User.hpp"
+#include <iostream>
 
-void NAMES(irc::Command *command) { (void)command; }
+std::string getUsersToString(irc::Channel channel)
+{
+	std::vector<irc::User *> users = channel.getUsers();
+	std::string users_string = "";
+
+	for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); ++it)
+	{
+		if (users_string.length())
+			users_string += " ";
+		if (channel.getUserMode(*(*it)).find('O') != std::string::npos)
+			users_string += "@";
+		users_string += (*it)->getNickname();
+	}
+	return users_string;
+}
+
+void NAMES(irc::Command *command)
+{
+	std::vector<irc::Channel *> channels = command->getServer().getChannels();
+	if (command->getParameters().size() == 1)
+	{
+		irc::Channel *channel = NULL;
+		for (std::vector<irc::Channel *>::iterator it = channels.begin(); it != channels.end(); ++it)
+			if ((*it)->getName() == command->getParameters()[0])
+			{
+				channel = *it;
+				break;
+			}
+		if (channel != NULL)
+		{
+			std::string channel_mode;
+			if (channel->getMode().find('p') != std::string::npos)
+				channel_mode = "*";
+			else if (channel->getMode().find('s') != std::string::npos)
+				channel_mode = "@";
+			else
+				channel_mode = "=";
+			command->reply(353, channel_mode, channel->getName(), getUsersToString(*channel));
+			command->reply(366, channel->getName());
+		}
+	}
+	else
+	{
+		for (std::vector<irc::Channel *>::iterator it = channels.begin(); it != channels.end(); ++it)
+		{
+			irc::Channel *channel = *it;
+			std::string channel_mode;
+			if (channel->getMode().find('p') != std::string::npos)
+				channel_mode = "*";
+			else if (channel->getMode().find('s') != std::string::npos)
+				channel_mode = "@";
+			else
+				channel_mode = "=";
+			command->reply(353, channel_mode, channel->getName(), getUsersToString(*channel));
+			command->reply(366, channel->getName());
+		}
+		std::vector<irc::User *> users = command->getServer().getUsers();
+		std::string users_not_in_channels = "";
+		for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); ++it)
+			if ((*it)->getLastChannel() == "*")
+			{
+				if (users_not_in_channels.length())
+					users_not_in_channels += " ";
+				users_not_in_channels += (*it)->getNickname();
+			}
+		if (users_not_in_channels.length())
+		{
+			command->reply(353, "", "*", users_not_in_channels);
+			command->reply(366, "*");
+		}
+	}
+}
