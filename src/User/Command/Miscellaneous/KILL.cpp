9e35741c8607858265a3a609837528fc3376099b
Assxios <droge@student.42.fr>
2021-12-14 21:21:29 +0100

    KILL command added, need to check some stuff on it with the boyz

--- src/User/Command/Miscellaneous/KILL.cpp
+++ src/User/Command/Miscellaneous/KILL.cpp
@@ -1,3 +1,25 @@
 #include "../Command.hpp"
+#include "../../User.hpp"
+#include "../../../Server/Server.hpp"
 
-void KILL(irc::Command *command) { (void)command; }
+void KILL(irc::Command *command)
+{
+	if (command->getParameters().size() == 0 || command->getTrailer().length() == 0)
+		return command->reply(461, "KILL");
+	if (command->getUser().getMode().find('o') == std::string::npos)
+		return command->reply(481);
+	irc::User *user = 0;
+	std::vector<irc::User *> users = command->getServer().getUsers();
+	for (std::vector<irc::User *>::iterator it = users.begin(); it != users.end(); ++it)
+	{
+		if ((*it)->getNickname() == command->getParameters()[0])
+		{
+			user = *it;
+			break ;
+		}
+	}
+	if (!user)
+		return command->reply(401, command->getParameters()[0]);
+	user->setDeleteMessage(command->getTrailer());
+	user->setStatus(irc::DELETE);
+}
