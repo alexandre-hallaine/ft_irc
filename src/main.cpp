55ef5180e6a6b0f1d58619c11d5b2ed0d00b9a5e
Nimon77 <nsimon@student.42.fr>
2021-12-15 10:25:46 +0100

    readd password in main

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	signal(SIGINT, handler);
 
 	server.getConfig().set("port", argv[1]);
-	//server.getConfig().set("password", argv[2]);
+	server.getConfig().set("password", argv[2]);
 
 	server.init();
 	while (!stop)
